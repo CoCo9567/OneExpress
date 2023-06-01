d8397fdefda3a8abac128cceb47194963f888b93
Yu Hui <2350553579@qq.com>
2023-06-01 19:27:43 +0800

    fix: xxxx

---
+++ craco.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+	devServer: {
+		proxy: {
+			'/api': {
+				target: 'http://192.168.121.66:8090/',
+				pathRewrite: {'^/api': ''},
+			},
+		},
+	},
+	webpack: {
+		alias: {
+			// "@":path.resolve(__dirname,)
+		},
+		plugins: {
+			add: [
+				/* ... */
+			],
+			remove: [
+				/* ... */
+			],
+		},
+	},
+};
